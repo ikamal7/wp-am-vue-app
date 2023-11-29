2c287a03dbb1bdb674e82bcf57bc6faaa1688a97
Kamal Hosen <kamalhosen8920@gmail.com>
2023-11-29 20:18:29 +0600

    Add All Components

---
+++ src/components/tabs/Tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="wp-am-vue-app-tabs">
+    <div class="tab-item">
+      <router-link to="/">
+        Settings
+      </router-link>
+    </div>
+    <div class="tab-item">
+      <router-link to="/table">
+        Table
+      </router-link>
+    </div>
+    <div class="tab-item">
+      <router-link to="/graph">
+        Graph
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./style.scss";
+
+export default {
+    name: "Tabs",
+    components: {},
+    data() {
+        return {};
+    },
+};
+</script>
